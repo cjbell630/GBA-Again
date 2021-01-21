e08dc80c646e412d916e3adee64df12040f128f9
Connor Bell <cjbell630@users.noreply.github.com>
2021-01-20 19:59:06 -0500

    bgscroll_test works

---
+++ bgscroll_test/images/santa_4tile.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(santa_4tile)
+
+//======================================================================
+//
+//	santa_4tile, 16x16@8, 
+//	+ palette 256 entries, not compressed
+//	+ 4 tiles not compressed
+//	Total size: 512 + 256 = 768
+//
+//	Time-stamp: 2021-01-19, 20:18:54
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_SANTA_4TILE_H
+#define GRIT_SANTA_4TILE_H
+
+#define santa_4tileTilesLen 256
+extern const unsigned short santa_4tileTiles[128];
+
+#define santa_4tilePalLen 512
+extern const unsigned short santa_4tilePal[256];
+
+#endif // GRIT_SANTA_4TILE_H
+
+//}}BLOCK(santa_4tile)
---
+++ bgscroll_test/images/santa_full.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(santa_full)
+
+//======================================================================
+//
+//	santa_full, 32x128@8, 
+//	+ palette 256 entries, not compressed
+//	+ 64 tiles not compressed
+//	Total size: 512 + 4096 = 4608
+//
+//	Time-stamp: 2021-01-19, 16:59:55
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_SANTA_FULL_H
+#define GRIT_SANTA_FULL_H
+
+#define santa_fullTilesLen 4096
+extern const unsigned short santa_fullTiles[2048];
+
+#define santa_fullPalLen 512
+extern const unsigned short santa_fullPal[256];
+
+#endif // GRIT_SANTA_FULL_H
+
+//}}BLOCK(santa_full)
